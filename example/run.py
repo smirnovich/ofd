b4f4cecffba26e371525e676f63f5070649a45bc
smirnovich <daniil_smirnov@yahoo.com>
2021-01-16 00:03:23 +0300

    Update run.py

--- example/run.py
+++ example/run.py
@@ -3,9 +3,11 @@ from vunit.verilog import VUnit
 
 VU = VUnit.from_argv()
 p = Path('..')
-
+print(p)
 lib = VU.add_library("lib")
 lib.add_source_files(p.resolve() / "example/*.sv", allow_empty=True)
+lib.add_source_files(p.resolve() / "ofd/example/*.sv", allow_empty=True)
+lib.add_source_files(p.resolve() / "*.sv", allow_empty=True)
 lib.add_source_files(p.resolve() / "hardware/buffer/srcs/*.sv", allow_empty=True)
 lib.add_source_files(p.resolve() / "hardware/core/*.sv", allow_empty=True)
 lib.add_source_files(p / "hardware/buffer/tb/*.sv", allow_empty=True)
