5a5066219e0b41d998bfddb07d11772520e7fd47
smirnovich <daniil_smirnov@yahoo.com>
2021-01-15 23:42:22 +0300

    Update run.py

--- example/run.py
+++ example/run.py
@@ -5,7 +5,7 @@ VU = VUnit.from_argv()
 p = Path('..')
 
 lib = VU.add_library("lib")
-lib.add_source_files(p.resolve() / "example/*.sv")
+lib.add_source_files(p.resolve() / "*.sv")
 lib.add_source_files(p.resolve() / "hardware/buffer/srcs/*.sv")
 lib.add_source_files(p / "hardware/buffer/tb/*.sv")
 lib.add_source_files(p.resolve() / "hardware/interfaces/uart/srcs/*.sv")
